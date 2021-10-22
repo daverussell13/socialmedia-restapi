b38b43e35c60f3cccce331e34e6e6c121688af29
daverussell13 <daverussell1309@gmail.com>
2021-10-22 14:19:49 +0700

    Add mongodb atlas connection

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 3000;
+
+/* MongoDb connection */
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to mongoDB");
+  }
+);
+
+/* Middleware */
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to port : ${PORT}`);
+});
