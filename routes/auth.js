4b590aa9c3a8a38542237d086a66ccd87bf312bc
daverussell13 <daverussell1309@gmail.com>
2021-10-22 18:14:37 +0700

    auth/register post request

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,30 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+/* Models */
+const User = require('../models/User');
 
 /* Register */
+router.post('/register', async (req,res) => {
+  try {
+    /* Hash Password using BCRYPT */
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password,salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      email: req.body.email
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch(err) {
+    res.status(400).json({
+      status: 'Bad request',
+      message: err.message
+    });
+  }
+})
 
 module.exports = router;
